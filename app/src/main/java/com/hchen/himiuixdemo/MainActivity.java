82102576b3bcf1bd08adf6e9bbf734d6128fdcea
HChenX <HChen_LXi@outlook.com>
2025-08-07 20:32:23 +0800

    Initial commit

---
+++ app/src/main/java/com/hchen/himiuixdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hchen.himiuixdemo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        getWindow().setNavigationBarContrastEnforced(false); // Xiaomi moment, this code must be here
+
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
