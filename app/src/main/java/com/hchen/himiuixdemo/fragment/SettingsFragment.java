e98313ede87b01c4d5a5d51ad52e4df6aca7e2a2
HChenX <HChen_LXi@outlook.com>
2025-08-08 19:09:46 +0800

    feat: Update HiMiuix

--- app/src/main/java/com/hchen/himiuixdemo/fragment/SettingsFragment.java
+++ app/src/main/java/com/hchen/himiuixdemo/fragment/SettingsFragment.java
@@ -12,6 +12,10 @@
 package com.hchen.himiuixdemo.fragment;
 
 
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.hchen.himiuix.callback.OnStateChangeListener;
+import com.hchen.himiuix.preference.MiuixSwitchPreference;
 import com.hchen.himiuixdemo.R;
 
 /**
@@ -27,6 +31,15 @@ public class SettingsFragment extends BasePreferenceFragment {
 
     @Override
     void initPrefs() {
-
+        MiuixSwitchPreference preference = findPreference("prefs_ui_switch");
+        preference.setOnStateChangeListener(new OnStateChangeListener() {
+            @Override
+            public boolean onStateChange(boolean newValue) {
+                if (newValue)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                return true;
+            }
+        });
     }
 }
